d8dfbffae602d5c8d02510f5fe246963e9a2d182
Mary1970 <skaya@na.edu>
2020-06-28 20:06:18 -0500

    first commit

---
+++ src/main/java/Driver.java
@@ -0,0 +1,10 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Driver {
+    public void mainTest() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+    }
+}
